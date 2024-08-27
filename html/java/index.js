09dc232ac6b78e9d1768d83b4301bd541bb307b2
Air Gwgie <arianahhemby@gmail.com>
2024-08-26 18:20:22 -0700

    form and functionality added

--- html/java/index.js
+++ html/java/index.js
@@ -1,12 +1,50 @@
-//footer vars
+//footer code
 var today = new Date();
 var thisYear = today.getFullYear();
 var footer = document.querySelector(".footer");
 var copyright = document.createElement("p");
 
-
-
-//footer code
 copyright.innerText = "Arianah Hemby " + thisYear;
 footer.appendChild(copyright);
+
+//messages code
+var messageForm = document.getElementsByName("leave_message")[0]; 
+
+messageForm.addEventListener("submit", function(event) {
+    event.preventDefault();
+
+    var userName = event.target.name.value;
+    var userEmail = event.target.email.value;
+    var message = event.target.userMessage.value;
+
+    console.log("Name is ", userName)
+    console.log("Email is ", userEmail);
+    console.log("Message is ", message);
+
+    var messageSection = document.getElementById("messages");
+    var messageList = messageSection.querySelector("ul");
+    var newMessage = document.createElement("li");
+
+    newMessage.innerHTML =  `
+        ${userName}</a> 
+        <a href="mailto:${userEmail}">${userEmail}</a> 
+        <span>${message}</span>
+    `;
+    
+    var removeButton = document.createElement("button");
+    removeButton.innerText = "remove";
+    removeButton.type = "button";
+
+    removeButton.addEventListener("click",function(event){
+        var entry = this.parentNode;
+        entry.remove();
+        
+
+    })
+    
+    newMessage.appendChild(removeButton);
+    messageList.appendChild(newMessage);
+
+    messageForm.reset()
+});
 
