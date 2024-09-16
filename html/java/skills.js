5efebc0374424a746967fad97877ecd9253e15d6
Air Gwgie <arianahhemby@gmail.com>
2024-09-15 17:34:26 -0700

    new css added

--- html/java/skills.js
+++ html/java/skills.js
@@ -1,11 +1,11 @@
 //skills vars
 var skills = ["Java", "Python", "Html", "CSS"];
 var skillsSelection = document.querySelector(".Skills");
-var skillsList = skillsSelection.querySelector("ul");
+var skillsList = skillsSelection.querySelector("div");
 
 // skills code
 for (k = 0; k < skills.length; k++){
-    var skill = document.createElement("li");
+    var skill = document.createElement("div");
     skill.innerText = skills[k];
     skillsList.appendChild(skill);
     
